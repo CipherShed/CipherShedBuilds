ecbeb0867a07ab9aa5079671b1db3ae7d0c98c05
Jason Pyeron <jpyeron@pdinc.us>
2015-02-21 07:48:51 -0500

    build "All"

---
+++ builds/00ea00e8e6a23a4243316f860aa07ed59203ab97/pyeron,jason/1424522781/src/Format/FormatCom_i.c
@@ -0,0 +1,85 @@
+
+
+/* this ALWAYS GENERATED file contains the IIDs and CLSIDs */
+
+/* link this file in with the server and any clients */
+
+
+ /* File created by MIDL compiler version 7.00.0555 */
+/* at Sat Feb 21 12:40:18 2015
+ */
+/* Compiler settings for .\FormatCom.idl:
+    Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
+    protocol : dce , ms_ext, c_ext, robust
+    error checks: allocation ref bounds_check enum stub_data 
+    VC __declspec() decoration level: 
+         __declspec(uuid()), __declspec(selectany), __declspec(novtable)
+         DECLSPEC_UUID(), MIDL_INTERFACE()
+*/
+/* @@MIDL_FILE_HEADING(  ) */
+
+#pragma warning( disable: 4049 )  /* more than 64k source lines */
+
+
+#ifdef __cplusplus
+extern "C"{
+#endif 
+
+
+#include <rpc.h>
+#include <rpcndr.h>
+
+#ifdef _MIDL_USE_GUIDDEF_
+
+#ifndef INITGUID
+#define INITGUID
+#include <guiddef.h>
+#undef INITGUID
+#else
+#include <guiddef.h>
+#endif
+
+#define MIDL_DEFINE_GUID(type,name,l,w1,w2,b1,b2,b3,b4,b5,b6,b7,b8) \
+        DEFINE_GUID(name,l,w1,w2,b1,b2,b3,b4,b5,b6,b7,b8)
+
+#else // !_MIDL_USE_GUIDDEF_
+
+#ifndef __IID_DEFINED__
+#define __IID_DEFINED__
+
+typedef struct _IID
+{
+    unsigned long x;
+    unsigned short s1;
+    unsigned short s2;
+    unsigned char  c[8];
+} IID;
+
+#endif // __IID_DEFINED__
+
+#ifndef CLSID_DEFINED
+#define CLSID_DEFINED
+typedef IID CLSID;
+#endif // CLSID_DEFINED
+
+#define MIDL_DEFINE_GUID(type,name,l,w1,w2,b1,b2,b3,b4,b5,b6,b7,b8) \
+        const type name = {l,w1,w2,{b1,b2,b3,b4,b5,b6,b7,b8}}
+
+#endif !_MIDL_USE_GUIDDEF_
+
+MIDL_DEFINE_GUID(IID, LIBID_CipherShedFormatCom,0xA7DF958C,0x0716,0x49E9,0x8C,0x3E,0x53,0xA7,0x75,0x79,0x75,0x76);
+
+
+MIDL_DEFINE_GUID(IID, IID_ICipherShedFormatCom,0x9EE02955,0x174A,0x48F1,0x82,0x0C,0x02,0x2F,0x32,0x7B,0xE1,0x09);
+
+
+MIDL_DEFINE_GUID(CLSID, CLSID_CipherShedFormatCom,0x777DCDFD,0xC330,0x480B,0xB5,0x82,0xB0,0x2B,0x57,0x58,0x0C,0xC9);
+
+#undef MIDL_DEFINE_GUID
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
